a0afa565d0172412379fab4855552e9aeccb401e
codej625 <codej625@gmail.com>
2022-11-13 23:29:41 +0900

    20221113 reset

--- devProject/src/main/java/com/dev/project/controller/HjController.java
+++ devProject/src/main/java/com/dev/project/controller/HjController.java
@@ -2,7 +2,6 @@ package com.dev.project.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 
 import com.dev.project.serviceimpl.HjServiceImpl;
 
@@ -12,9 +11,4 @@ public class HjController {
 	@Autowired
 	HjServiceImpl hjServiceImpl;
 
-	@GetMapping(value = "/")
-	public String name() throws Exception {
-		
-		return "/page/test";
-	}
 }

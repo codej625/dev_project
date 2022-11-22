ea8af650840a80504f8ccc9e72b23ce0fe963aaf
codej625 <codej625@gmail.com>
2022-11-22 18:57:06 +0900

    20221122 source update

--- devProject/src/main/java/com/dev/project/controller/HjController.java
+++ devProject/src/main/java/com/dev/project/controller/HjController.java
@@ -2,13 +2,18 @@ package com.dev.project.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.dev.project.serviceimpl.HjServiceImpl;
 
+import lombok.extern.log4j.Log4j2;
+
+@Log4j2
 @Controller
 public class HjController {
 
-	@Autowired
-	HjServiceImpl hjServiceImpl;
-
+    @Autowired
+    HjServiceImpl hjServiceImpl;
 }

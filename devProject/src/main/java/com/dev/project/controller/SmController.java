ddf549dec64061921f2f993cc4c21445d679e807
codej625 <codej625@gmail.com>
2022-11-22 19:14:30 +0900

    20221122 update

--- devProject/src/main/java/com/dev/project/controller/SmController.java
+++ devProject/src/main/java/com/dev/project/controller/SmController.java
@@ -5,6 +5,9 @@ import org.springframework.stereotype.Controller;
 
 import com.dev.project.serviceimpl.SmServiceImpl;
 
+import lombok.extern.log4j.Log4j2;
+
+@Log4j2
 @Controller
 public class SmController {
 	
--- devProject/src/main/java/com/dev/project/serviceimpl/SmServiceImpl.java
+++ devProject/src/main/java/com/dev/project/serviceimpl/SmServiceImpl.java
@@ -1,14 +1,19 @@
 package com.dev.project.serviceimpl;
 
 import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.stereotype.Service;
 
 import com.dev.project.mapper.SmMapper;
 import com.dev.project.service.SmService;
 
+import lombok.extern.log4j.Log4j2;
+
+@Log4j2
 @Service
 public class SmServiceImpl implements SmService {
 
-	@Autowired
-	SmMapper smMapper;
+    @Autowired
+    SmMapper smMapper;
+
 }

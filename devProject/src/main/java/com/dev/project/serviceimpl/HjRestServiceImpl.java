f3b3fc8bc41b0771dfb59eba1109e7a954fb170e
codej625 <codej625@gmail.com>
2022-11-22 19:16:19 +0900

    20221122 first commit

--- devProject/src/main/java/com/dev/project/serviceimpl/HjRestServiceImpl.java
+++ devProject/src/main/java/com/dev/project/serviceimpl/HjRestServiceImpl.java
@@ -1,14 +1,12 @@
 package com.dev.project.serviceimpl;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
 
 import com.dev.project.mapper.HjRestMapper;
 import com.dev.project.service.HjRestService;
 
-@Service
 public class HjRestServiceImpl implements HjRestService {
 
-	@Autowired
-	HjRestMapper hjRestMapper;
+    @Autowired
+    HjRestMapper hjRestMapper;
 }
